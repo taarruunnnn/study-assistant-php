c506471f89e09bf2068e159fe94bc981226ad8ff
Dave Howson <dave.sohan@gmail.com>
2018-12-30 19:03:58 +0530

    Sidebar UI changes

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,32 +1,46 @@
 {{-- Sidebar Holder --}}
 <nav id="sidebar">
     <div class="sidebar-header">
-        <a href="{{ route('dashboard') }}"><h3>Study Assistant</h3></a>
+    <a href="{{ route('dashboard') }}">
+        <img src="{{ asset('storage/logo.png') }}" alt="">
+        <span id="logo-text">Study Assistant</span>
+    </a>
     </div>
 
     <ul class="list-unstyled components">
         <li class="{{ Request::is('dashboard') ? 'active' : '' }}">
-            <a href="{{ route('dashboard') }}">Dashboard</a>
+            <a href="{{ route('dashboard') }}">
+                <i class="fas fa-tachometer-alt sidebar-icon"></i>
+                <span class="sidebar-text">Dashboard</span>
+            </a>
         </li>
-        <li class="{{ Request::is('schedules/*') ? 'active' : '' }}">
-            <a href="/schedules">Schedules</a>
+        <li class="{{ Request::is('schedules*') ? 'active' : '' }}">
+            <a href="/schedules">
+                <i class="fas fa-calendar-alt sidebar-icon"></i>
+                <span class="sidebar-text">Schedules</span>
+            </a>
+        </li>
+        <li class="{{ Request::is('reports*') ? 'active' : '' }}">
+            <a href="{{ route('reports.show') }}">
+                <i class="fas fa-chart-line sidebar-icon"></i>
+                <span class="sidebar-text">Reports</span>
+            </a>
         </li>
         <li class="{{ Request::is('user/*') ? 'active' : '' }}">
-            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">User</a>
-            <ul class="collapse list-unstyled" id="pageSubmenu">
-                <li class="{{ Request::is('user/edit') ? 'active' : '' }}">
-                    <a href="/user/edit">Edit User</a>
-                </li>
-            </ul>
+            <a href="/user/edit">
+                <i class="fas fa-cog sidebar-icon"></i>
+                <span class="sidebar-text">Settings</span>
+            </a>
         </li>
-        <li>
-            <a href="{{ route('reports.show') }}">Reports</a>
-        </li>
+        
     </ul>
     <div id="CTA">
         <ul class="list-unstyled">
-            <li>
-                <a href="/session" class="btn btn-success">START STUDY SESSION</a>
+            <li class="{{ Request::is('session/*') ? 'active' : '' }}">
+                <a href="/session">
+                    <i class="fas fa-clock sidebar-icon"></i>
+                    <span class="sidebar-text">Session</span>
+                </a>
             </li>
         </ul>
     </div>
